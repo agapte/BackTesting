6c6a5b7731d4fd3dc94633a5d204a45571a03373
agapte <apteag@gmail.com>
2018-02-06 19:55:43 +0530

    Added data

---
+++ src/com/bt/util/DataAppender.java
@@ -0,0 +1,57 @@
+package com.bt.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+
+public class DataAppender {
+	
+	public static void main(String[] args) throws Exception {
+		File file = new File("C:\\Users\\Amit\\Downloads\\drive-download-20180117T143526Z-001");
+		File niftyOut = new File("C:\\BackTesting\\niftyOut.csv");
+		File bankNiftyOut = new File("C:\\BackTesting\\BANKNIFTY_F1.txt");
+		niftyOut.createNewFile();
+		bankNiftyOut.createNewFile();
+		PrintWriter printWriter = new PrintWriter(niftyOut);
+		PrintWriter bankNiftyWriter = new PrintWriter(bankNiftyOut);
+		File[] listFiles = file.listFiles();
+		for (File dateFolder : listFiles) {
+			if(dateFolder.isDirectory())
+			{
+				File[] dataFiles = dateFolder.listFiles();
+				for (File dataFile : dataFiles) {
+					if(dataFile.getName().equals("NIFTY_F1.txt"))
+					{
+						System.out.println(dataFile.getAbsolutePath());
+						BufferedReader br = new BufferedReader(new FileReader(dataFile));
+						 String line = null;
+						 while ((line = br.readLine()) != null) {
+							 printWriter.write(line);
+							 printWriter.write(System.getProperty("line.separator"));
+						 }
+						 br.close();
+					}
+					if(dataFile.getName().equals("BANKNIFTY_F1.txt"))
+					{
+						System.out.println(dataFile.getAbsolutePath());
+						BufferedReader br = new BufferedReader(new FileReader(dataFile));
+						 String line = null;
+						 while ((line = br.readLine()) != null) {
+							 bankNiftyWriter.write(line);
+							 bankNiftyWriter.write(System.getProperty("line.separator"));
+						 }
+						 br.close();
+					}
+					
+				}
+			}
+		}
+		printWriter.flush();
+		printWriter.close();
+		bankNiftyWriter.flush();
+		bankNiftyWriter.close();
+	}
+
+}
